ec3d616aa63f9cf3cb33067068f921d707d2a17f
kimjunoh <kimjo8317@gmail.com>
2023-05-16 02:07:53 +0900

    로그인 기능 구현

---
+++ src/main/java/hello/login/web/login/LoginService.java
@@ -0,0 +1,32 @@
+package hello.login.web.login;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+    private final MemberRepository memberRepository;
+
+    /*@return이 null일시 로그인실패 / optional에 get으로 멤버값 불러와짐*/
+    public Member login(String loginId,String password) {
+//        Optional<Member> findMemberOptional = memberRepository.findByLoginId(loginId);
+//        Member member = findMemberOptional.get();
+//        if (member.getPassword().equals(password)) {
+//            return member;
+//        } else {
+//            return null;
+//        }
+
+    /*ctrl+alt+t refactoring*/
+//    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+}
+
