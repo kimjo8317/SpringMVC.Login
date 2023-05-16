7108ea4e0364c7e300a84277ab4e42710fe3e73f
kimjunoh <kimjo8317@gmail.com>
2023-05-16 23:44:21 +0900

    Session Cookie 기능

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,42 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+//    @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping
+    String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+
+        if(memberId == null) {
+            return "home";
+        }
+        //로그인 (db에서 찾은 member가 없으면 홈으로)
+        Member loginMember = memberRepository.findId(memberId);
+        if (loginMember == null) {
+            return "home";
+        }
+        //로그인성공 로직
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+
     }
 }
--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Controller
@@ -26,17 +28,33 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult) {
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
-        Member logonMember = loginService.login(form.getLoginId(), form.getPassword());
-        if (logonMember == null) {
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+        if (loginMember == null) {
             bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다");
             return "login/loginForm";
         }
-        /*로그인 성공 처리 TODO*/
+        /*로그인 성공 처리 */
+        /*시간정보를 안주면 session cookie*/
+        /*쿠키 생성 로직*/
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+        response.addCookie(idCookie);
         return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expireCookie(response,"memberId");
+        return "redirect:/";
+    }
+
+    private static void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 
 }
